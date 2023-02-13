2cc00be2df1d28e336cf75cf3050a882fdc15eaa
Gojo <91662751+Gojo54@users.noreply.github.com>
2023-02-13 23:20:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 
 const router = jsonServer.router('./DataBase/item.json');
 const middlewares = jsonServer.defaults();
-
+const port = process.env.PORT || 3001
 server.use(middlewares);
 
 server.use((req,res,next)=>{
@@ -12,7 +12,7 @@ server.use((req,res,next)=>{
 
 server.use(router);
 
-server.listen(3000, ()=>console.log("Json server is started on POST 3000"))
+server.listen(port, ()=>console.log("Json server is started on POST 3000"))
 
 
 
