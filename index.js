1c43e44d313dee3a726eab83e54638a5f129845f
Gojo <91662751+Gojo54@users.noreply.github.com>
2023-02-13 23:49:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 
-const router = jsonServer.router('./DataBase/item.json');
+const router = jsonServer.router('./dataBase/item.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001
 server.use(middlewares);
